39ff566b4c8c6f507c3fb4f3b26b8b682abf6d76
yoav2001 <yoavelkana@gmail.com>
2021-08-24 16:19:41 +0300

    create api arr and user req

---
+++ api/userApi.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+
+
+app.post('/login', (req, res, next) => {
+
+    // Authenticate User
+
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    if (accessToken === null)
+        next(new ApiError(401, 'the user isnt connect'))
+
+    res.json({ accessToken: accessToken })
+})
