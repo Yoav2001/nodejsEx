835a98b1b660382b344a3068a885c9c9909b4ee7
yoav2001 <yoavelkana@gmail.com>
2021-09-05 16:43:04 +0300

    chane the main router from app to router

--- api/mainRouter.js
+++ api/mainRouter.js
@@ -1,37 +1,59 @@
-require('dotenv').config()
-const arr = [4, 5, 6, 7];
-const express = require('express')
-const app = express();
-const jwt = require('jsonwebtoken')
-const apiErrorHandler = require('../error/api-error-handler');
-const ApiError = require("../error/apiError")
+// require('dotenv').config()
 
-const arrayRouter = require("../api/arrayRouter")
-const userRouter = require("../api/userRouter")
+
+import express from 'express';
+import arrayRouter from './arrayRouter.js'
+const app = express();
+const router = express.Router();
+
+const arr = [4, 5, 6, 7];
+// const express = require('express')
+// const app = express();
+// const jwt = require('jsonwebtoken')
+// const apiErrorHandler = require('../error/api-error-handler');
+// const ApiError = require("../error/apiError")
+
+// const arrayRouter = require("../api/arrayRouter")
 
 // app.use(bodyparser.json());
+app.set('view engine', 'jade');
 
 app.use(express.json());
 
+router.route("/")
+    .get((req, res) => {
+        const date = new Date().toJSON().slice(0, 10);
 
-
-app.get("/", (req, res) => {
-    const date = new Date().toJSON().slice(0, 10);
-
-    res.json({
-        msg: "Hello " + req.user.name + " today is " + date,
-    });
-});
-app.get("/echo", (req, res) => {
-
-    const msg = req.query.msg;
-    res.json({
-        echo: "The message is " + msg
+        res.json({
+            msg: "Hello " + req.user.name + " today is " + date,
+        });
     })
-});
 
 
-app.use("/array/", arrayRouter);
+router.route("/echo")
+    .get((req, res) => {
+        const msg = req.query.msg;
+        res.json({
+            echo: "The message is " + msg
+        })
+    })
 
-// export default app;
-module.exports = app;
+// app.get("/", (req, res) => {
+//     const date = new Date().toJSON().slice(0, 10);
+
+//     res.json({
+//         msg: "Hello " + req.user.name + " today is " + date,
+//     });
+// });
+// app.get("/echo", (req, res) => {
+
+//     const msg = req.query.msg;
+//     res.json({
+//         echo: "The message is " + msg
+//     })
+// });
+
+router.use("/array/", arrayRouter)
+    // app.use("/array/", arrayRouter);
+
+export default router;
